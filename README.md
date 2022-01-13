da49d85bd6a9039875c634f5c78aa427ce9e46a6
Anish Sinha <anishsinha0128@gmail.com>
2022-01-13 00:58:25 -0500

    removed useless library and added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Threaded Tree
+
+This repository contains an implementation for a single threaded binary search tree in ANSI C. That's pretty much it.
--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "linkedlist/linkedlist.h"
 #include "threadedtree/threadedtree.h"
-#include <stdio.h>
 
 int main() {
     List *list1 = array_to_list((double *) &(double[]) {1, 2, 3, 4, 5, 6, 7, 8}, 8);
